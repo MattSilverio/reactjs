78b995ed8162d7f461b776488b3d8452eecdb4a4
Matheus Silverio <matheusphillipo@gmail.com>
2022-07-11 14:33:22 -0300

    Upload component Post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,29 +1,47 @@
-import styles from './Post.module.css';
+import styles from "./Post.module.css";
 
-export function Post(){
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/MattSilverio.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Matheus Phillipo</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
 
-    return(
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/MattSilverio.png" />
-                    <div className={styles.authorInfo}>
-                        <strong>Matheus Phillipo</strong>
-                        <span>Web Developer</span>
-                    </div>
-                </div>
+        <time title="30 de Junho de 2022" dateTime="2022-06-30 01:57">
+          Publicado há 1h
+        </time>
+      </header>
 
+      <div className={styles.content}>
+        <p>
+          Fala Galera!!
+        </p>
 
-                <time title='30 de Junho de 2022' dateTime="2022-06-30 01:57">Publicado há 1h</time>
-            </header>
+        <p>Acabei de subir um projeto no meu sorteio. É um projeto que fiz no NLW Return, evento da Rocketseat</p>
+        <p><a href="">jane.design/doctorcare</a></p>
 
-            <div className={styles.content}>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo numquam eius 
-                    excepturi dolore doloribus quasi provident consequatur harum tempora, eaque iste, 
-                    deleniti veniam commodi a corporis ullam incidunt quae mollitia?
-                </p>
-            </div>
-        </article>
-    )
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário"/>
+
+        <button type="submit">Comentar</button>
+      </form>
+    </article>
+  );
 }
