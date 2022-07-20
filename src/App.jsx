75b6c94a22d90cf6a6684b9ae0019261c68d4c64
Matheus Phillipo Silverio Silva <matheusphillipo@gmail.com>
2022-07-20 01:12:13 -0300

    Iteração do JSX

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,37 @@ import styles from './App.module.css'
 import { Post } from "./components/Post/Post";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'http://www.github.com/MattSilverio.png',
+      name: "Matheus Silverio",
+      role: "Estudante @ UnB"
+    },
+    content: [
+        {type: 'paragraph' , content: 'Fala Galera!!'},
+        {type: 'paragraph' , content: 'Acabei de subir um projeto no meu portifolio. É um projeto que fiz no NLW Return, evento da Rocketseat'},
+        {type: 'link', content: 'jane.design/doctorcare'},     
+    ],
+    publishedAt: new Date('2022-07-20 22:27:44'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'http://www.github.com/Iagorrr04.png',
+      name: "Iago Rocha",
+      role: "Estudante @ UnB"
+    },
+    content: [
+        {type: 'paragraph' , content: 'Fala Galera!!'},
+        {type: 'paragraph' , content: 'Acabei de concluir mais um contest em C++, chequem as soluções no meu repositório. Logo mais tem vídeo'},
+        {type: 'link', content: 'github.com/Iagorrr04/Competitive-Programming-Algorithms'},     
+    ],
+    publishedAt: new Date('2022-07-15 20:04:57'),
+  }
+]
+
 export default function App(){
   return (
     <div>
@@ -13,15 +44,15 @@ export default function App(){
         <Sidebar />
         
         <main>
-          <Post
-            author={"Matheus Silverio"}
-            content={"Eae"}
-          />
-
-          <Post
-            author={"Matheus Silverio"}
-            content={"Eae"}
-          />
+          {posts.map((post) => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,37 +1,42 @@
+import {format, formatDistanceToNow} from "date-fns"
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from "../Avatar/Avatar";
 import { Comment } from "../Comment/Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post({author, content, publishedAt}) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH'h'mm'm'", {locale: ptBR});
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+  
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar hasBorder={true} src="https://github.com/MattSilverio.png" />
+          <Avatar hasBorder={true} src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Matheus Phillipo</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="30 de Junho de 2022" dateTime="2022-06-30 01:57">
-          Publicado há 1h
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>
-          Fala Galera!!
-        </p>
-
-        <p>Acabei de subir um projeto no meu sorteio. É um projeto que fiz no NLW Return, evento da Rocketseat</p>
-        <p><a href="">jane.design/doctorcare</a></p>
-
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map((line) =>{
+          if(line.type === 'paragraph'){
+            return <p>{line.content}</p>
+          }else if(line.type === 'link'){
+            return <p><a href='#'>{line.content}</a></p>
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
